36f6cee408506095223c55863934f9c48d0703b6
madhavi <ivaturi.madhavi@gmail.com>
2015-05-11 02:35:28 -0400

    java_favourites_updated

--- app/src/main/java/com/example/madhavi/spraguelib/Favorites.java
+++ app/src/main/java/com/example/madhavi/spraguelib/Favorites.java
@@ -1,5 +1,6 @@
 package com.example.madhavi.spraguelib;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.SparseBooleanArray;
@@ -126,7 +127,56 @@ public class Favorites extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.catalog) {
+            Intent i = new Intent(Favorites.this.getApplicationContext(), Catalog.class);
+            startActivity(i);
+            return true;
+        } else if (id == R.id.services) {
+            if(LogActivity.loginflag==0)
+            {
+                MyDialogFragment dialog;
+                dialog = new MyDialogFragment();
+                dialog.show(getFragmentManager(), "MyDialogFragmentTag");
+            }
+            else {
+                Intent i = new Intent(Favorites.this.getApplicationContext(), Services.class);
+                startActivity(i);
+            }
+            return true;
+        }  else if (id == R.id.help) {
+            if(LogActivity.loginflag==0)
+            {
+                MyDialogFragment dialog;
+                dialog = new MyDialogFragment();
+                dialog.show(getFragmentManager(), "MyDialogFragmentTag");
+            }
+
+            else {
+                Intent i = new Intent(Favorites.this.getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+            return true;
+        }  else if (id == R.id.notifications) {
+            if(LogActivity.loginflag==0)
+            {
+                MyDialogFragment dialog;
+
+
+                dialog = new MyDialogFragment();
+                dialog.show(getFragmentManager(), "MyDialogFragmentTag");
+            }
+            else {
+                Intent i = new Intent(Favorites.this.getApplicationContext(), Notifications.class);
+                startActivity(i);
+            }
+            return true;
+        }  else if (id == R.id.library_info) {
+            Intent i = new Intent(Favorites.this.getApplicationContext(), LibraryNews.class);
+            startActivity(i);
+            return true;
+        }        if (id == R.id.create_datab) {
+            Intent i = new Intent(Favorites.this.getApplicationContext(), MainPage.class);
+            startActivity(i);
             return true;
         }
 
